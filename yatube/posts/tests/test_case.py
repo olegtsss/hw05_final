a19cc0198681ae005c4ec8ad13dcda7c3fff50c5
Олег Суворов <admin@olegtsss.ru>
2022-10-19 23:52:57 +0600

    Работа по тесту временна приостановлена

--- yatube/posts/tests/test_case.py
+++ yatube/posts/tests/test_case.py
@@ -43,6 +43,14 @@ class BaseCaseForTests(TestCase):
         b'\x02\x00\x01\x00\x00\x02\x02\x0C'
         b'\x0A\x00\x3B'
     )
+    OTHER_GIF = (
+        b'\x47\x49\x46\x38\x39\x61\x02\x00'
+        b'\x01\x00\x80\x00\x00\x00\x00\x00'
+        b'\xFF\xFF\xFF\x21\xF9\x04\x00\x00'
+        b'\x00\x00\x00\x2C\x00\x00\x00\x00'
+        b'\x02\x00\x01\x00\x00\x02\x02\x0C'
+        b'\x0A\x00\x3B'
+    )
 
     @classmethod
     def setUpClass(cls):
@@ -66,7 +74,8 @@ class BaseCaseForTests(TestCase):
             name='small.gif', content=cls.GIF, content_type='image/gif'
         )
         cls.GIF_ANOTHER_FILE = SimpleUploadedFile(
-            name='small_another.gif', content=cls.GIF, content_type='image/gif'
+            name='small_another.gif', content=cls.OTHER_GIF,
+            content_type='image/gif'
         )
         cls.post = Post.objects.create(
             text='Первый пост.',
